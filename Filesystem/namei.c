dec81a9aa7bbea1ae5a875e00a3ed887506bf0db
Lars De Pauw <lars.de.pauw@hotmail.com>
2020-11-09 18:14:38 +0100

    extra commit

--- Filesystem/namei.c
+++ Filesystem/namei.c
@@ -1,5 +1,6 @@
 /*
  *      namei.c         met eventuele stub : om te kunnen testen 
+ 	Bram Lauwens
  */
 
 #include <sys/types.h>
@@ -10,52 +11,184 @@
 #include <string.h>
 #define EXT extern
 #include "hfs.h"
+#include <ctype.h>
 
-
-//#ifdef STUB
 short namei(char *cpn)
 {
-	static Inode	ipb;
-	Inode	*ip;
-	char	*cpl;
-	short wdir = 0;
 
-	printf(" diract: offset in dir : "); scanf("%hd%*c", &u.u_diract);
-	printf(" parent inode nummer : "); scanf("%hd%*c", &u.u_pdir);
-	printf(" inode nummer : "); scanf("%hd%*c", &wdir);
-	if ( wdir != 0 )
-	{
-		ip = LeesInode(wdir, &ipb);
-		if ( verbose && ip )
-		{
-			printf("%3d : ", wdir);
-			DrukInode(ip);
-		}
-		u.u_dirent.d_ino = wdir;
-	}
-	else
-	{
-		ip = (struct inode *)NULL;
-		u.u_dirent.d_ino = 0;
-	}
-	/* afsplitsen van de laatste component van de padnaam */
-	cpl = strrchr(cpn, '/');
-	if ( cpl == NULL ) 
-		cpl = cpn;
-	else
-		cpl++;
+    if (verbose) {
+        printf("NAMEI \n");
+        for (int i = 0; i< 20; i++) {
+            printf("%c", cpn[i]);
+        }
+        printf("char %c \n", cpn);
+        
+    }
+	
+	Inode werkinode;
+	int werkinodenummer;
+	int teller;
+	int naam=0;
+	int intelezenblokken;
+	char buf[BLOKSIZE];
+	Dir *dirp;
+	int terug=1;
+	int lus;
+	
+	// if padnaam start bij root = starten met /
 	memset(u.u_dirent.d_naam, '\0', NAMELEN);
-	strncpy(u.u_dirent.d_naam, cpl, NAMELEN);
-	if ( verbose )
-		printf("dirent %d %-7.7s   diract %d   pdir %d\n",
-			u.u_dirent.d_ino, u.u_dirent.d_naam, u.u_diract, u.u_pdir); 
-	u.u_error = 0;
-	return wdir;
+	u.u_diract=0;
+	if( cpn[0] =='/')
+	{
+		
+        if (verbose) {printf("gestart bij de root van pad \n");}
+		// werkinode = rootinode
+		werkinodenummer=u.u_rdir; // is 1
+		teller=1;
+        if (verbose) {printf(" werkinodenummer is %d \n", werkinodenummer);}
+	}
+	else
+	{
+        if (verbose) {printf(" niet gestart bij de root van pad \n");}
+		// werkinode = inode van actuele directory
+		werkinodenummer=u.u_cdir; // is 0
+		teller =0;
+        if (verbose) {printf(" werkinodenummer is %d \n", werkinodenummer);}
+	}
+	
+	// nu lezen we inode
+	LeesInode(werkinodenummer, &werkinode);
+	// DrukInode(werkinode);
+	
+	// testen of inode een directory is
+	if ((werkinode.i_mode & S_IFMT) != S_IFDIR)
+	{
+        if (verbose) {printf(" ai geen directory \n");}
+		u.u_error=GEENDIR;
+		return -1;
+	} 
+	
+	// zolang er nog een padnaam is, zolang er een / is
+	//while( cpn[teller] != '\0')
+    // voor makdir kan het ook een \0 zijn
+    while(!isdigit(cpn[teller]) && cpn[teller] != '\0')
+	{
+		
+		// lees volgende component van padnaam uit input
+		//printf(" er is nog padnaam met charchter %c \n" , cpn[teller]);
+		
+		memset(u.u_dirent.d_naam, '\0', NAMELEN);
+		while ( cpn[teller] != '/')
+		{
+			// proberen nu woord per woord in te lezen en te kopieren
+			u.u_dirent.d_naam[naam] = cpn[teller];
+            if (verbose) {printf(" de teller van naam is %d met waarde %c  \n ", naam, u.u_dirent.d_naam[naam]);}
+			teller++;
+			naam++;
+			if (cpn[teller] == '\0')
+			{
+				break;
+			}
+		}
+		teller++;
+		// nu hebben we in u.u_dirent.d_naam[naam] bijvoorbeeld temp staan
+		// zoeken naar de innode, inlezen van innode 0 of 1 zit in werkinode
+		// goed kijken of de i_size kleiner is dan 32, of 64,...
+		
+		intelezenblokken=(werkinode.i_size/BLOKSIZE); 
+		if ( werkinode.i_size%BLOKSIZE != 0)
+		{
+			intelezenblokken++;
+            if (verbose) {printf("eentje bijgeteld");}
+		}
+        if (verbose) {printf(" intelezenblokken is %d \n", intelezenblokken);}
+		
+		u.u_pdir=werkinodenummer;
+		for(int i=0;i<intelezenblokken;i++)
+		{
+            if (verbose) {printf("for lus in te lezen blokken");}
+			if ((werkinode.i_mode & S_IFMT) != S_IFDIR)
+						{
+                            if (verbose) {printf(" ai geen directory \n");}
+							u.u_error=GEENDIR;
+							u.u_pdir=1;
+							//u.u_pdir= de oude; niet nodig mss lijn 100 aanpassen naar hierna pas
+							return -1;
+						} 
+			//u.u_pdir=werkinodenummer;
+			LeesBlok(werkinode.i_blok[i], buf);
+			DrukBlok(buf,BLOKSIZE); // 32 is bij default
+			// output: 1 . 0 0 0 0 0 0 1 . . 0 0 0 0 0 0 j e f 0 0 0 0 3 t m p 0 0 0 0
+			
+			
+			// ingelezen in array buf en typecasten naar een dir structuur
+			dirp=(Dir*) buf;
+			// nakijken of het overeenkomt
+			
+            if (verbose) {printf("u.u_dirent.d_naam is %s \n", u.u_dirent.d_naam);}
+			
+			terug=-1;
+            if (verbose) {printf("werkinode i size is %d \n",werkinode.i_size);}
+			
+			int verschil= werkinode.i_size - i*BLOKSIZE;
+			if (verschil<BLOKSIZE)
+			{
+				lus=werkinode.i_size%BLOKSIZE;
+			}
+			else
+				lus=BLOKSIZE;
+				
+			if (intelezenblokken>1)
+			{
+				lus=werkinode.i_size-BLOKSIZE;
+			}
+			for( int j=0; j<=lus/DIRLEN;j++)
+			{
+                if (verbose) {
+                    printf("j is %d \n", j);
+                    printf("dirp->d_naam is %s \n", dirp->d_naam);
+                }
+				if (strncmp(u.u_dirent.d_naam, dirp->d_naam,NAMELEN)==0)
+				{
+					if ( dirp->d_ino != 0)
+					{
+                        if (verbose) {printf("gevonden en inode niet 0\n");}
+						werkinodenummer=dirp->d_ino;
+						
+						u.u_diract=j*DIRLEN;
+						LeesInode(werkinodenummer, &werkinode);
+						
+						naam=0;
+						terug=werkinodenummer;
+					}
+					//else
+                    if ( dirp->d_ino == 0)
+					{
+                        if (verbose) {printf("deze is recent verwijderd en innodde is 0\n");}
+						u.u_diract=j*DIRLEN;
+						terug=0;
+					}
+				}
+				else
+				{
+                    if (verbose) {printf("nog niet gevonden \n");}
+				}
+				if ( dirp->d_ino == 0)
+				{
+					u.u_diract=j*DIRLEN;
+                    terug = 0;
+				}
+				dirp++;
+			}
+		}
+	}
+	
+    if (verbose) {
+        printf ("namei goed verlopen met eindresultaat %d \n ", terug);
+        printf("namei bestand  %s  %c \n", cpn,cpn[0]);
+        printf("u.u_diract: %d",u.u_diract);
+    }
+	return terug;
+	
 }
-/*#else
-short namei(char *cpn)
-{
-	printf("namei bestand  %s \n", cpn);
-	return 1;
-}
-#endif*/
+
